41bc5353effc440d2759240255cc8f20282c2dd7
captain-mike <info@grafikweb.it>
2022-07-04 10:50:02 +0200

    js

---
+++ m1/w3/d1/js/script.js
@@ -0,0 +1,16 @@
+document.write('Hello World!');
+document.write('Hello World!');
+
+document.getElementById('test').innerHTML = 'Ciao'
+document.getElementById('test').style.backgroundColor = 'red'
+
+
+//console.log('%c ciao','color: red')
+console.log('posso scrivere quello che voglio')
+
+
+//commento ad un sola riga
+/*
+Commento su
+più righe
+*/
