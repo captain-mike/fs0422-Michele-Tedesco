37a657036767e776262141f7cd5b86c9bf1e5d13
captain-mike <info@grafikweb.it>
2022-09-14 10:58:29 +0200

    feature module

---
+++ m3/w3/d3/angular1/src/app/primi/primi.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrimiListComponent } from './primi-list/primi-list.component';
+import { PrimiDetailComponent } from './primi-detail/primi-detail.component';
+import { PrimiItemComponent } from './primi-item/primi-item.component';
+import { PrimiComponent } from './primi/primi.component';
+import { PrimiRoutingModule } from './primi-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    PrimiListComponent,
+    PrimiDetailComponent,
+    PrimiItemComponent,
+    PrimiComponent
+  ],
+  imports: [
+    PrimiRoutingModule,
+    SharedModule
+  ],
+})
+export class PrimiModule { }
